b3c05bcc63002ad563dca7688d27249fb47bfd67
jpbmdev <jpbmdev@gmail.com>
2021-08-17 18:51:53 -0500

    :art: redux initial state

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Recorder from '../Recorder/Recorder';
+import Calendar from '../Calendar/Calendar';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      Hello
+      <Recorder />
+      <Calendar />
     </div>
   );
 }
